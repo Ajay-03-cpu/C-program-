79e92b94a50a863de373e1c23bd04b97a0bb3d77
Ajay-03-cpu <ajayshanmugam2006@gmail.com>
2026-02-09 12:03:23 +0530

    Update variable printing to show initial and changed values
    Added print statements to show initial and changed values of Num1.

--- C Practice- Change variable value and print updated value.c
+++ C Practice- Change variable value and print updated value.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main(){
     int Num1 = 10;
+    printf("initial value");
+    printf("%d",Num1);
     Num1= 20;
-    
+    printf("changed Value")
     printf("%d",Num1);
    
     return 0;
+
 }
