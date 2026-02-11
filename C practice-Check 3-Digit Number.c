14d16ff968065dbd1c6f25e4e646880bcf3bb1e2
Ajay-03-cpu <ajayshanmugam2006@gmail.com>
2026-02-11 15:33:17 +0530

    Add files via upload

---
+++ C practice-Check 3-Digit Number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+   int num;
+   
+   scanf("%d",&num);
+   
+   if (num>=100 && num<=999){
+       printf("Three digit number");}
+   
+   
+   else{
+       printf("Not three digit number");
+   }
+   
+   return 0;
+}
