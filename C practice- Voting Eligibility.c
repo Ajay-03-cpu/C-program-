17661511c46835622cf92d85f6ac4c7d3eafc55e
Ajay-03-cpu <ajayshanmugam2006@gmail.com>
2026-02-11 15:25:27 +0530

    Add files via upload

---
+++ C practice- Voting Eligibility.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+   int age;
+   
+   scanf("%d",&age);
+   
+   if (age>=18){
+       printf("Eligible to vote");}
+   
+   
+   else{
+       printf("Not eligible to vote");
+   }
+   
+   return 0;
+}
