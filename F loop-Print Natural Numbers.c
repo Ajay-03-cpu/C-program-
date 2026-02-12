96f8d2b7e3b6119244a5ba096f399e670a0db110
Ajay-03-cpu <ajayshanmugam2006@gmail.com>
2026-02-12 16:03:51 +0530

    Add files via upload

---
+++ F loop-Print Natural Numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i,range;
+     scanf("%d",&range);
+    if(range>0&&range<=1000){
+   
+    for (i=1;i<=range;i++){
+        printf("%d ",i);
+    }}
+    return 0;
+}
