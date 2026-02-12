1f116b4bd79c42a6b1cc0d816f6605ed8929fbbc
Ajay-03-cpu <ajayshanmugam2006@gmail.com>
2026-02-12 14:19:01 +0530

    Add files via upload

---
+++ C practice- Menu-Driven Program.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main() {
+    int choice,num1,num2;
+    printf("1.Sum\n 2.Difference\n 3.Product\n 4.Divide\n");
+    scanf("%d %d %d",&choice ,&num1,&num2);
+    switch(choice){
+    case 1:printf("%d",num1+num2);break;
+    case 2:printf("%d",num1-num2);break;
+    case 3:printf("%d",num1*num2);break;
+    case 4:printf("%d",num1/num2);break;}
+    return 0;
+}
