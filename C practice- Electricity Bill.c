b04871992222006e2634f0373843014be22374ec
Ajay-03-cpu <ajayshanmugam2006@gmail.com>
2026-02-12 15:28:14 +0530

    Improve electricity bill calculation logic
    Refactor electricity bill calculation logic for clarity and correctness.

--- C practice- Electricity Bill.c
+++ C practice- Electricity Bill.c
@@ -2,33 +2,18 @@
 int main() {
   int units,bill=0 ;
   scanf("%d",&units);
- 
  if(units<=50) {
     bill+=units*3;
       printf("%d\n",bill);
  }
+else if(units<=100){
+    bill+=(50*3)+(units-50)*5;
+    (units-50)*10;
+    printf("%d",bill);
+}
 else{
-    bill+=50*3;
-    units-=50;
-   
-   
+    bill=(50*3)+(50*5)+(units-100)*10;
 }
- if(units<50){
- bill+=50*5;
-      
- }
- else{
- bill+=50*5;
- units-=50;
-     
- }
- if(units>0){
-     
-    bill+=units*10;
-}
-
-    
-printf("%d",bill);
 
     return 0;
 }
