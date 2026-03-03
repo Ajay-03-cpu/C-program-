b933522d9a0feb37f4b64545989116cbca96efb7
Ajay-03-cpu <ajayshanmugam2006@gmail.com>
2026-03-03 20:13:34 +0530

    Add files via upload

---
+++ Array-removing duplicates.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) {
+        scanf("%d",&a[i]);
+    }
+    for (int i=0;i<n;i++) {
+        int flag=1;
+        for (int j=0;j<i;j++){
+            if (a[i]==a[j]){
+                flag=0;
+                break;
+            }
+        }
+        if (flag==1){
+            printf("%d ", a[i]);
+        }
+    }
+    return 0;
+}
